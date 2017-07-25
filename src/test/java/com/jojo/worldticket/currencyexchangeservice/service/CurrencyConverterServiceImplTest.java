f715ab18c0b59471f158d25b098b23b5c3090894
keattiwut KOSITTARUK <keattiwut.KOSITTARUK@amadeus.com>
2017-07-25 14:57:19 +0700

    add more teste

--- src/test/java/com/jojo/worldticket/currencyexchangeservice/service/CurrencyConverterServiceImplTest.java
+++ src/test/java/com/jojo/worldticket/currencyexchangeservice/service/CurrencyConverterServiceImplTest.java
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.modules.junit4.PowerMockRunner;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -31,37 +32,66 @@ public class CurrencyConverterServiceImplTest {
     @Mock
     ExchangeRateRepository exchangeRateRepository;
 
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
+    @Test
+    public void convertCurrency_withSameCurrency_ShouldReturnGivenAmount() throws Exception {
+        when(exchangeRateRepository.findTopByNameOrderByEffectiveDateDesc(anyString()))
+                .thenReturn(mockExchangeRate("THB", "33.41000"));
 
-    @After
-    public void tearDown() throws Exception {
-        currencyConverterService = null;
-        exchangeRateRepository = null;
+        BigDecimal amount = new BigDecimal("1000");
+        String fromCurrency = "USD";
+        String toCurrency = "USD";
+        ConverterResponse result = currencyConverterService
+                .convertCurrency(amount, fromCurrency, toCurrency);
+        assertEquals(new BigDecimal("1000"), result.getAmount());
     }
 
     @Test
-    public void convertCurrency() throws Exception {
+    public void convertCurrency_withBaseCurrencyToOtherCurrency_ShouldConvertToOtherCurrency() throws Exception {
         when(exchangeRateRepository.findTopByNameOrderByEffectiveDateDesc(anyString()))
-                .thenReturn(mockExchangeRate());
+                .thenReturn(mockExchangeRate("THB", "33.41000"));
 
         BigDecimal amount = new BigDecimal("1000");
         String fromCurrency = "USD";
         String toCurrency = "THB";
         ConverterResponse result = currencyConverterService
                 .convertCurrency(amount, fromCurrency, toCurrency);
-        assertEquals("33410", result.getAmount());
+        assertEquals(new BigDecimal("33410.00000"), result.getAmount());
     }
 
-    private ExchangeRate mockExchangeRate() {
+    @Test
+    public void convertCurrency_withOtherCurrencyTOBase_ShouldConvertToBaseCurrency() throws Exception {
+        when(exchangeRateRepository.findTopByNameOrderByEffectiveDateDesc(anyString()))
+                .thenReturn(mockExchangeRate("THB", "33.41000"));
+
+        BigDecimal amount = new BigDecimal("33410.00000");
+        String fromCurrency = "THB";
+        String toCurrency = "USD";
+        ConverterResponse result = currencyConverterService
+                .convertCurrency(amount, fromCurrency, toCurrency);
+        assertEquals(new BigDecimal("1000.00"), result.getAmount());
+    }
+
+    @Test
+    public void convertCurrency_withOtherToOther_ShouldConvertToOther() throws Exception {
+        when(exchangeRateRepository.findTopByNameOrderByEffectiveDateDesc(anyString()))
+                .thenReturn(mockExchangeRate("JPY", "110"))
+                .thenReturn(mockExchangeRate("THB", "33.41000"));
+
+        BigDecimal amount = new BigDecimal("1000");
+        String fromCurrency = "JPY";
+        String toCurrency = "THB";
+        ConverterResponse result = currencyConverterService
+                .convertCurrency(amount, fromCurrency, toCurrency);
+        assertEquals(new BigDecimal("303.6969000"), result.getAmount());
+    }
+
+    private ExchangeRate mockExchangeRate(String currency, String price) {
         ExchangeRate rate = new ExchangeRate();
         rate.setId(Long.valueOf(1));
-        rate.setName("USD/THB");
+        rate.setName("USD/"+currency);
         rate.setEffectiveDate(new Date());
         rate.setSymbol("THB=X");
-        rate.setPrice(new BigDecimal("33.41000"));
+        rate.setPrice(new BigDecimal(price));
         return rate;
     }
 
